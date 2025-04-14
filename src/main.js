5d26736f7e3c67a049ccc6dab2433724a95698ce
ElireChara <vova200517@bk.ru>
2025-04-15 00:06:59 +0300

    свайпер недоделан

--- src/main.js
+++ src/main.js
@@ -1,2 +1,36 @@
 import './assets/styles/main.scss';
-// тут может быть логика для слайдера, меню, и т.д.
+import Swiper from 'swiper';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+
+
+const elements = document.querySelectorAll('.animate-on-scroll');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('opacity-100', 'translate-y-0');
+      entry.target.classList.remove('opacity-0', 'translate-y-10');
+    }
+  });
+});
+elements.forEach(el => observer.observe(el));
+
+
+Swiper.use([Navigation]);
+
+const productSwiper = new Swiper('.productSwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    480: {
+      slidesPerView: 1,
+    },
+  },
+});
