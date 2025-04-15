10d6c79f932a35921fffe371eb199a89e97da1b6
ElireChara <vova200517@bk.ru>
2025-04-15 09:24:39 +0300

    свайпер

--- src/main.js
+++ src/main.js
@@ -1,36 +1,11 @@
 import './assets/styles/main.scss';
-import Swiper from 'swiper';
+import Swiper, {navigation}  from 'swiper';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 
-
-const elements = document.querySelectorAll('.animate-on-scroll');
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('opacity-100', 'translate-y-0');
-      entry.target.classList.remove('opacity-0', 'translate-y-10');
-    }
-  });
-});
-elements.forEach(el => observer.observe(el));
-
-
-Swiper.use([Navigation]);
-
-const productSwiper = new Swiper('.productSwiper', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
+const swiper = new Swiper('.sample-slider', {   
+    navigation: {                      
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    480: {
-      slidesPerView: 1,
-    },
-  },
-});
+})
