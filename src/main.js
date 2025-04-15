edf45999d8b82d70ef104f0cc5d6a80ae4e864e8
ElireChara <vova200517@bk.ru>
2025-04-15 12:27:32 +0300

    полурабочий слайдер

--- src/main.js
+++ src/main.js
@@ -3,9 +3,27 @@ import Swiper, {navigation}  from 'swiper';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 
-const swiper = new Swiper('.sample-slider', {   
-    navigation: {                      
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-})
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      640: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  });
+
+  // Кастомная навигация
+  document.querySelector(".prev-btn").addEventListener("click", () => {
+    swiper.slidePrev();
+  });
+
+  document.querySelector(".next-btn").addEventListener("click", () => {
+    swiper.slideNext();
+  });
